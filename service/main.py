8df5ac5ce4deeb55eb8dd74b463e3ade24a26197
Fred Zhen <fred.zhen@gmail.com>
2023-01-24 12:08:17 +0800

    created service in python fastapi

---
+++ service/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from bson import ObjectId
+from typing import List
+import motor.motor_asyncio
+
+app = FastAPI()
+db_client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
+d3 = db_client.d3
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class DishesModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
+    orders: int = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "veg soup",
+                "orders": 200
+            }
+        }
+
+
+@app.get('/dishes', response_model=List[DishesModel])
+async def get_all_dishes():
+    dish_list = []
+    cursor = d3.dishes.find()
+    async for dish in cursor:
+        dish_list.append(dish)
+    return dish_list
+
---
+++ service/service.md
@@ -0,0 +1,11 @@
+Service
+=======
+
+### project setup:
+1. create a new pure python project from the terminal/MacOS (Don't use IDE template create the project);
+2. create a virtual environment: `python -m venv venv`;
+3. activate virtual environment: `source venv/bin/activate` (deactivate virtual environment: `deactivate`);
+   >Note: all python and pip command have already been aliased to python3 and pip3 in `.zshrc`;
+4. copy the requirements of a fast api project and run `pip install -r service_rqms.txt`
+   >run this either in terminal directly or in pycharm terminal window;
+5. open all python file in pycharm and make sure all the dependencies are installed.
