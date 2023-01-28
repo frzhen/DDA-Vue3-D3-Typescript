81a520b9e9f1b5c88f00b73f08f51c01d4120c6e
Fred Zhen <fred.zhen@gmail.com>
2023-01-28 18:06:58 +0800

    Created create budget API in service and create the transitions for donut chart

--- service/main.py
+++ service/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field
-from bson import ObjectId
 from typing import List
 import motor.motor_asyncio
 
@@ -9,38 +9,6 @@ db_client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
 d3 = db_client.d3
 
 
-class PyObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError("Invalid objectid")
-        return ObjectId(v)
-
-    @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type="string")
-
-
-# class DishesModel(BaseModel):
-#     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-#     name: str = Field(...)
-#     orders: int = Field(...)
-#
-#     class Config:
-#         allow_population_by_field_name = True
-#         arbitrary_types_allowed = True
-#         json_encoders = {ObjectId: str}
-#         schema_extra = {
-#             "example": {
-#                 "name": "veg soup",
-#                 "orders": 200
-#             }
-#         }
-
 class ResponseDishModel(BaseModel):
     name: str = Field(...)
     orders: int = Field(...)
@@ -48,6 +16,18 @@ class ResponseDishModel(BaseModel):
 class ResponseBudgetModel(BaseModel):
     name: str = Field(...)
     amount: int = Field(...)
+
+
+class CreateBudgetSchema(BaseModel):
+    name: str = Field(...)
+    amount: int = Field(..., gt=0)
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "snacks",
+                "amount": 155
+            }
+        }
 
 @app.get('/dishes', response_model=List[ResponseDishModel])
 async def get_all_dishes():
@@ -65,4 +45,15 @@ async def get_budgets():
     async for budget in cursor:
         budget_item = { 'name': budget['name'], 'amount': budget['amount']}
         budget_list.append(budget_item)
-        return budget_list
+    return budget_list
+
+@app.post("/budget")
+async def create_a_budget(budget: CreateBudgetSchema = Body(...)):
+    data = jsonable_encoder(budget)
+    print(data)
+    print(type(data))
+    result = await d3.budgets.insert_one(data)
+    print(f' result: {result.inserted_id}')
+    new_budget = await d3.budgets.find_one({"_id": result.inserted_id})
+    return {"status_code": 200, "name": new_budget["name"], "message": "budget added"}
+
--- web/src/d3/donut-chart.ts
+++ web/src/d3/donut-chart.ts
@@ -4,13 +4,95 @@
  * @Email: fred.zhen@gmail.com
  */
 import * as d3 from "d3";
+import {arc} from "d3";
+import sharedWorkerConstructor from "*?sharedworker";
+
 
 export function createDonutChart(data: any) {
+
+  const dims = {
+    height: 300,
+    width: 300,
+    radius: 150
+  };
+
+  const cent = {
+    x: (dims.width / 2 + 80 ),
+    y: (dims.height /2 + 80 )
+  };
+
   const svg = d3.select('#donutChart')
     .append('svg')
-    .attr('width', 600)
-    .attr('height', 600);
+    .attr('width', dims.width + 150)
+    .attr('height', dims.height + 150)
 
-  svg.append("rect").text(data[0].name);
+  const graph = svg.append('g')
+    .attr('transform', `translate(${cent.x}, ${cent.y})`);
+
+  const pie = d3.pie()
+    .sort(null)
+    .value((d: any) => d.amount);
+
+  const arcPath: any = d3.arc()
+    .outerRadius(dims.radius)
+    .innerRadius(dims.radius - 50);
+
+  // custom color pattern
+  const colorRange = [
+    '#6668aa',
+    '#323D5E',
+    '#41D0F0',
+    '#FC8C3C',
+    '#28AE60',
+    '#2980b9',
+    '#E281EC',
+    '#5E6484'
+  ]
+  const arcColor = d3.scaleOrdinal(colorRange)
+    .domain(data.map((d: any) => d.name));
+
+  // custom tween for transition
+  const arcTweenEnter: any = (d: any) => {
+    let i = d3.interpolate(d.endAngle, d.startAngle)
+
+    return function(ticker: any) {
+      d.startAngle = i(ticker);
+      return arcPath(d);
+    }
+  }
+  const arcTweenExit: any = (d: any) => {
+    let i = d3.interpolate(d.startAngle, d.endAngle);
+
+    return function(ticker: any) {
+      d.startAngle = i(ticker);
+      return arcPath(d);
+    }
+  }
+  // use function keyword  to allow use of 'this'
+  // function  arcTweenUpdate(d: any) {
+  //   let i = d3.interpolate(this._current, d);
+  //   this._current = i(1);
+  //   return function(ticker: any) {
+  //     return arcPath(i(ticker));
+  // }
+
+  const paths = graph.selectAll('path').data(pie(data));
+
+  paths.exit()
+    .transition().duration(1500)
+    .attrTween('d', arcTweenExit)
+    .remove();
+
+  paths.transition().duration(1500)
+    .attrTween('d', arcTweenEnter);
+
+  paths.enter()
+    .append('path')
+    .attr('stroke', '#fff')
+    .attr('stroke-width', 0)
+    .attr('fill', (d: any)=> arcColor(d.data.name))
+    .attr('fill-opacity', 0.7)
+    .transition().duration(1500)
+    .attrTween('d', arcTweenEnter);
 
 }
