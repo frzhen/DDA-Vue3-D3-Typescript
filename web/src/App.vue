9561a2a80160d1e8ac42e2cbf88d35d5e042f08f
Fred Zhen <fred.zhen@gmail.com>
2023-02-02 20:43:17 +0800

    Restructured asset folder with new svg directory

--- web/src/App.vue
+++ web/src/App.vue
@@ -8,24 +8,24 @@ import D3Page from "./views/D3Page.vue";
 
 <template>
   <div class="box">
-    <img alt="Vue logo" src="./assets/logo.svg" width="200" height="200" />
+    <img alt="Vue logo" src="./assets/svg/logo.svg" width="200" height="200" />
     <HomeContent msg="This is a D3.js project with Vue3 written in Typescript" />
   </div>
   <div class="box">
     <span>
-      <img alt="total icon" src="./assets/total.svg" width="60" height="60" />
+      <img alt="total icon" src="./assets/svg/total.svg" width="60" height="60" />
     </span>
     <span>
-      <img alt="used icon" src="./assets/used.svg" width="60" height="60" />
+      <img alt="used icon" src="./assets/svg/used.svg" width="60" height="60" />
     </span>
     <span>
-      <img alt="remain icon" src="./assets/remain.svg" width="60" height="60" />
+      <img alt="remain icon" src="./assets/svg/remain.svg" width="60" height="60" />
     </span>
     <span>
-      <img alt="expired icon" src="./assets/expired.svg" width="60" height="60" />
+      <img alt="expired icon" src="./assets/svg/expired.svg" width="60" height="60" />
     </span>
     <span>
-      <img alt="ratio icon" src="./assets/ratio.svg" width="60" height="60" />
+      <img alt="ratio icon" src="./assets/svg/ratio.svg" width="60" height="60" />
     </span>
   </div>
   <D3Page />
